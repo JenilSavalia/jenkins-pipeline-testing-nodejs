7f4bd964b61a61e215f1433a716129a73cecf9d4
Jenil Savalia <jenil.savalia.cd@gmail.com>
2026-05-01 11:28:31 +0530

    Initial CRUD app

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let items = [];
+let id = 1;
+
+// CREATE
+app.post('/items', (req, res) => {
+  const item = { id: id++, name: req.body.name };
+  items.push(item);
+  res.status(201).json(item);
+});
+
+// READ
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+// UPDATE
+app.put('/items/:id', (req, res) => {
+  const item = items.find(i => i.id == req.params.id);
+  if (!item) return res.status(404).send('Not found');
+
+  item.name = req.body.name;
+  res.json(item);
+});
+
+// DELETE
+app.delete('/items/:id', (req, res) => {
+  items = items.filter(i => i.id != req.params.id);
+  res.send('Deleted');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
